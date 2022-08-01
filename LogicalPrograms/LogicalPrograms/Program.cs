504087bc68bf16693095184b0fd7c7d90790c451
pratik <pratikbhandurge2222@gmail.com>
2022-08-01 15:05:45 +0530

    Fibonacci

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -5,16 +5,16 @@ namespace LogicalProgramming
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome To Logical Programming!");
+        {          
+            //Console.WriteLine("Welcome To Logical Programming!");
 
-            //.Console.WriteLine("Fibonacci Series Program");
-            //Fibonacci f = new Fibonacci();
-            //f.FindFibonacci();
+            .Console.WriteLine("Fibonacci Series Program");
+            Fibonacci f = new Fibonacci();
+            f.FindFibonacci();
 
-            Console.WriteLine("Perfect Number Program!");
-            PerfectNumber pf = new PerfectNumber();
-            pf.perfectNumber();
+            //Console.WriteLine("Perfect Number Program!");
+            //PerfectNumber pf = new PerfectNumber();
+            //pf.perfectNumber();
 
             //Console.WriteLine("Prime Number Program!");
             //PrimeNumber pn = new PrimeNumber();
